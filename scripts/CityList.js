a5bc634b3163073fb3a4fa7d61b4234a52edf5cf
Chaneice Jackson <c.jackson0126@gmail.com>
2021-11-16 11:41:15 -0600

    first commit

---
+++ scripts/CityList.js
@@ -0,0 +1,21 @@
+import { getWalkers } from "./database.js"
+
+const walkers = getWalkers()
+
+
+export const CityList = () => {
+    let citiesHTML = "<ol>"
+
+    for (const walker of walkers) {
+        citiesHTML += `<li>${walker.city}</li>`
+    }
+
+    citiesHTML += "</ol>"
+
+    return citiesHTML
+}
+
+
+
+
+//There is 3 bugs in this code.//
